423e6ea167ceb48b9b33afaf1b73f7974eb2bcf6
hopeswanke <hopeswanke@gmail.com>
2020-11-06 16:09:19 -0500

    update 11/6

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
+// global imports
 import React from "react";
 import { useState } from "react";
-import AboutMe from "./about.js";
-import MainPage from "./main.js";
-import Projects from "./projects.js";
-import Job from "./job.js";
-import Wander from "./wander.js";
+// function imports for my pages
+import AboutMe from "./pages/about.js";
+import MainPage from "./pages/main.js";
+import Projects from "./pages/projects.js";
+import Job from "./pages/job.js";
+import Wander from "./pages/wander.js";
+// css import
 import "./App.css";
 
 function App() {
@@ -25,26 +28,40 @@ function App() {
   function handleClickWander() {
     setPage(<Wander />);
   }
-  //---------------------- changes page to home page -----------------------------
+  // ---------------------- changes page to home page -----------------------------
   function handleClickHome() {
     setPage(<MainPage />);
   }
   return (
+    // containing body
     <div id="body">
       <body>
+      {/*  ----------------------------------- HEADER -------------------------------------- */}
+        {/* header and when click brings you to home page */}
         <h1 class="hope" onClick={handleClickHome}>Hope Swanke</h1>
+        {/*  --------------------------------- NAV BAR ------------------------------------ */}
+        {/* nav bar container */}
         <div id="nav-bar">
+          {/* on click brings you to the About me page */}
           <h3 class="pages" onClick={handleClickAbout}>About Me</h3>
+          {/* on click brings you to my projects page */}
           <h3 class="pages" onClick={handleClickProjects}>My Projects</h3>
+          {/* on click brings you to my work page */}
           <h3 class="pages" onClick={handleClickJob}>Work History</h3>
+          {/* on click brings you to my wanderlust page */}
           <h3 class="pages" onClick={handleClickWander}>Wanderlust</h3>
         </div>
-        
+        {/* ------------------------------------ MAIN SECTION ------------------------------------ */}
+        {/* variable that sets the page depending on which function is clicked */}
         {page}
+        {/* containing footer */}
+        {/* ------------------------------------- FOOTER ------------------------------------ */}
         <div id="footer">
+          {/* footer title */}
           <h2 id="footer-title">Contact:</h2>
           <div id="contact-info">
-            <a href="https://www.linkedin.com/in/hope-swanke-aa653b112/" target="_blank"><h3 class="footer-info" >LinkinedIn</h3></a>
+            {/* clickable links that bring you to my linkedIn, instagram, and my printed email */}
+            <a href="https://www.linkedin.com/in/hope-swanke-aa653b112/" target="_blank"><h3 class="footer-info" >LinkedIn</h3></a>
             <a href="https://www.instagram.com/hopeswanke/" target="_blank"><h3 class="footer-info" >Instagram</h3></a>
             <h3 class="footer-info" >hopeswanke@gmail.com</h3>
           </div>
--- client/src/pages/main.js
+++ client/src/pages/main.js
@@ -9,7 +9,7 @@ function MainPage() {
       <div id="pic-content">
         <div><img class="main-photos" src="../images/alice.jpg" /><h2 class="main-caption">My friend Alice and I in Liverpool, England</h2></div>
         <div><img class="main-photos" src="../images/italy/good-mountain.jpg" /><h2 class="main-caption">Skiing in the Italian Alps</h2></div>
-        <div><img class="main-photos" src="../images/france/scuba-gear.jpg" /><h2 class="main-caption">Scuba diving of the coast of France</h2></div>
+        <div><img class="main-photos" src="../images/france/scuba-gear.JPG" /><h2 class="main-caption">Scuba diving of the coast of France</h2></div>
         <div><img class="main-photos" src="../images/kittybed.jpg" />
         <h2 class="main-caption">My Beloved Steve</h2></div>
       </div>
